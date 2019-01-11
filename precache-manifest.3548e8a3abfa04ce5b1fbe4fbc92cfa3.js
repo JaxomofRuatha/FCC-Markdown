fdcf03be0068c2f98cd8060d94064ee899048b07
John Chritton <jchritton33@gmail.com>
2019-01-10 21:54:26 -0600

    Updates

---
+++ precache-manifest.3548e8a3abfa04ce5b1fbe4fbc92cfa3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "620cdbce25d723b98d12",
+    "url": "/jc-markdown/static/css/main.9e22215c.chunk.css"
+  },
+  {
+    "revision": "620cdbce25d723b98d12",
+    "url": "/jc-markdown/static/js/main.620cdbce.chunk.js"
+  },
+  {
+    "revision": "a005ae2cf0cc0d1fcaaa",
+    "url": "/jc-markdown/static/css/1.3cc3ea1c.chunk.css"
+  },
+  {
+    "revision": "a005ae2cf0cc0d1fcaaa",
+    "url": "/jc-markdown/static/js/1.a005ae2c.chunk.js"
+  },
+  {
+    "revision": "132b89997c10b218219a",
+    "url": "/jc-markdown/static/js/runtime~main.132b8999.js"
+  },
+  {
+    "revision": "bd364aca1815c8da1727a6334cddf123",
+    "url": "/jc-markdown/index.html"
+  }
+];
